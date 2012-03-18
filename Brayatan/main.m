0c8049bc2cc79085c721d3a929cd3c9c016be89b
Aldrin Martoq <aldrin@martoq.cl>
2012-03-18 18:08:23 -0300

    Initial Commit

---
+++ Brayatan/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Brayatan
+//
+//  Created by Aldrin Martoq on 3/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

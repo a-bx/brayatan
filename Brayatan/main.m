832c2b2ef96a284a8463f1d5f6b6a4269aa36577
Aldrin Martoq <aldrin@martoq.cl>
2012-03-26 14:49:18 -0300

    Multicore/singlecore example in the same main.m

--- Brayatan/main.m
+++ Brayatan/main.m
@@ -19,18 +19,36 @@ int fibonnacci(int n) {
     return fibonnacci(n - 2) + fibonnacci(n - 1);
 }
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {        
-        NSLog(@"Hello, World!");
-        
-        Http *http = [Http createServerWithIP:@"127.0.0.1" atPort:8000 callback:^(Request *req, Response *res) {
+Http *multicore() {
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+
+    Http *http = [Http createServerWithIP:@"127.0.0.1" atPort:8000 callback:^(Request *req, Response *res) {
+        dispatch_async(queue, ^{
             res.status = 200;
             [res.headers setObject:@"Content-type" forKey:@"text/plain"];
-            [res endWithBody:[NSString stringWithFormat:@"Hello Client number %d fibonnaci is: %d\n", count++, fibonnacci(20)]];
-        }];
+            [res endWithBody:[NSString stringWithFormat:@"Hello Client number %d fibonnaci is: %d\n", count++, fibonnacci(1)]];
+        });
+    }];
+    
+    return http;
+}
 
-        NSLog(@"http: %@", http);
+Http *singlecore() {
+    Http *http = [Http createServerWithIP:@"127.0.0.1" atPort:8000 callback:^(Request *req, Response *res) {
+        res.status = 200;
+        [res.headers setObject:@"Content-type" forKey:@"text/plain"];
+        [res endWithBody:[NSString stringWithFormat:@"Hello Client number %d fibonnaci is: %d\n", count++, fibonnacci(1)]];
+    }];
+    
+    return http;
+}
 
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {        
+        Http *http = singlecore();
+
+        NSLog(@"Hello flaites, http: %@", http);
         uv_run(uv_default_loop());
     }
     return 0;
